3e03fd6b1d5deb42d0cbf58d8d6ae746c64a5191
Pushpa Kumari <153999245+pushpakumari98@users.noreply.github.com>
2024-06-11 18:26:01 +0530

    Add files via upload

---
+++ EnumerateFunc.py
@@ -0,0 +1,14 @@
+marks=[12,56,32,98,45,1,4]
+
+index=0
+for mark in marks:
+    print(mark)
+    if(index==3):
+         print("Harry,awesome")
+    index+=1
+
+
+# for index,mark in enumerate(marks):
+#       print(mark)
+#       if(index==3):
+#           print("Harry,awesome!")
